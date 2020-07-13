231c86038e7e40a25bd7e5a447abeed8127a2978
daniel70125 <dwrighttt504@gmail.com>
2020-07-13 11:36:46 -0500

    Updated Routes

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+import Landing from './Components/Landing/Landing';
+import USMap from './components/USMap/USMap';
+import GetInvolved from './components/GetInvolved/GetInvolved'
+
+export default (
+    <Switch>
+        <Route exact path = '/' component={Landing} />
+        <Route path='/map' component={USMap} />
+        <Route path='/getinvolved' component={GetInvolved} />
+    </Switch>
+)
