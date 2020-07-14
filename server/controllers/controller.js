22aed1f47f1f7e949190e3c957abb02f7d2fc0c6
Jeff Palmer <jeff@palmytree.com>
2020-07-14 18:03:34 -0500

    wrote prison data endpoint

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -1,10 +1,26 @@
+const prisonData = require('./data/prisonPop2018.json')
+
 module.exports = {
-  // example: async (req, res) => {
-  //   const db = req.app.get('db')
+	prisonData: (req, res) => {
+		let output
 
-  //   db.fbi_data.insert({
-  //     state_abv: 'TX',
-
-  //   })
-  // }
+		if (Object.keys(req.query).length) {
+			for (let query in req.query) {
+				switch (query) {
+					case 'state':
+						if (prisonData[req.query.state]) {
+							output = prisonData[req.query.state]
+						} else {
+							return res.status(404).send('State not found')
+						}
+						break
+					default:
+						return res.status(400).send('Query parameter not found')
+				}
+			}
+		} else {
+			output = prisonData
+		}
+		res.status(200).send(output)
+	}
 }
