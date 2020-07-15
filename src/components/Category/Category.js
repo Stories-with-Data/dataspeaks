d38c1adbd1d0ac67c07cb8d647c658b785be9f7b
Josh <gibbons.josh@outlook.com>
2020-07-15 15:37:00 -0600

    Update Category

---
+++ src/components/Category/Category.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Category(props) {
+  return (
+    <div className='categoryContainer'>
+      
+    </div>
+  )
+}
+
+export default Category
--- src/components/State/State.js
+++ src/components/State/State.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Category from '../Category/Category'
 
 function State(props) {
 
   return (
     <div>
       <h1>This is an individual state</h1>
+      <Category data={props.data}/>
       <button className='button' onClick={() => props.handleStateClose()}>State Close</button>
     </div>
   )
