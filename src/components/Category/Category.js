85dd5adb7377572ba141997ab5e498fe557025ea
Josh <gibbons.josh@outlook.com>
2020-07-15 16:50:23 -0600

    Added basic category and state

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import './Category.css'
+// import Rank from '../Rank/Rank'
+// import Chart from '../Chart/Chart'
 
 function Category(props) {
   return (
     <div className='categoryContainer'>
-      
+      <p>Category Component</p>
+      {/* <Rank rank={props.rank}/> */}
+      {/* <Chart/> */}
     </div>
   )
 }
--- src/components/State/State.js
+++ src/components/State/State.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import Category from '../Category/Category'
+import './State.css'
 
 function State(props) {
 
   return (
-    <div>
-      <h1>This is an individual state</h1>
-      <Category data={props.data}/>
+    <div className='stateContainer'>
+      <h1>State Name</h1>
+      <div className='categoryColumnContainer'>
+        <Category data={props.data}/>
+        <Category data={props.data}/>
+        <Category data={props.data}/>
+      </div>
       <button className='button' onClick={() => props.handleStateClose()}>State Close</button>
     </div>
   )
