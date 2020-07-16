a8573636b75f841b70e1e8db3e83c6281567a8e5
Eliottnibley <e.nibley@gmail.com>
2020-07-16 17:00:50 -0600

    update

--- src/components/BarGraph/BarGraph.js
+++ src/components/BarGraph/BarGraph.js
@@ -1,12 +1,39 @@
 import React, {Component} from 'react'
 import * as d3 from 'd3'
+import './BarGraph.css'
 
-function BarGraph (props) {
-  return (
-    <div className='bar-graph-container'>
-      this is a bar graph
+// 0: {label: "White", value: 0.35, tooltip: "Description of data"}
+// 1: {label: "Black", value: 0.05, tooltip: "Description of data"}
+// 2: {label: "Hispanic", value: 0.3, tooltip: "Description of data"}
+// 3: {label: "Asian", value: 0.33, tooltip: "Description of data"}
+// 4: {label: "Other", value: 0.3, tooltip: "Description of data"}
+
+class BarGraph extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: []
+    }
+  }
+  
+    
+  componentDidMount () {
+    this.setState({data: this.props.data})
+
+    this.drawChart()
+  }
+
+  drawChart () {
+    
+  }
+
+  render () {
+    console.log(this.state.data)
+    return (
+    <div className='bar-chart-container'>
+      
     </div>
-  )
+  )}
 }
 
 export default BarGraph;
--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -4,13 +4,12 @@ import BarGraph from '../BarGraph/BarGraph'
 
 function Chart(props) {
   const {chartData} = props
-  // console.log(chartData[0])
   return (
   <div className='chartMain'>
     {/* <BarChart data={chartData}/> */}
     {chartData.map((elem, ind) => {
       if(elem.chartType === 'bar'){
-        return <BarGraph key={ind} data={elem.data}/>
+        return <BarGraph key={ind} data={elem}/>
       }
     })}
   </div>
