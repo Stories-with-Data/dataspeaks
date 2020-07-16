ab89d32cdb9bf39d6e4f4dd97c7475085f6746bc
Josh <gibbons.josh@outlook.com>
2020-07-16 16:50:15 -0600

    Graph testing

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -4,12 +4,13 @@ import BarGraph from '../BarGraph/BarGraph'
 
 function Chart(props) {
   const {chartData} = props
-  // console.log(chartData[0])
+  // console.log(chartData)
   return (
   <div className='chartMain'>
     {/* <BarChart data={chartData}/> */}
     {chartData.map((elem, ind) => {
       if(elem.chartType === 'bar'){
+        console.log('check')
         return <BarGraph key={ind} data={elem.data}/>
       }
     })}
