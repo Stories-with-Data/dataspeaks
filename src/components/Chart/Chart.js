e831fd541d7569326f1178e5bf6fcb51e1fb60fa
Eliottnibley <e.nibley@gmail.com>
2020-07-16 12:36:46 -0600

    update

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import './Chart.css'
+import BarGraph from '../BarGraph/BarGraph'
 
 function Chart(props) {
   const {chartData} = props
+  // console.log(chartData[0])
   return (
   <div className='chartMain'>
-    
+    {/* <BarChart data={chartData}/> */}
+    {chartData.map((elem, ind) => {
+      if(elem.chartType === 'bar'){
+        return <BarGraph key={ind} data={elem.data}/>
+      }
+    })}
   </div>
   )
 }
