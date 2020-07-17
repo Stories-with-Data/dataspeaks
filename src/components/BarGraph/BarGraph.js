d7bbcc96b2c016429676b43aa278e21d64c447e9
Josh <gibbons.josh@outlook.com>
2020-07-17 11:24:54 -0600

    BarGraph update

--- src/components/BarGraph/BarGraph.js
+++ src/components/BarGraph/BarGraph.js
@@ -1,43 +1,50 @@
 import React, { Component } from 'react';
-
-class BarGraph extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      data: []
-    }
-  }
-  componentDidMount() {
-    console.log(this.props.data)
-    this.setState({data: this.props.data})
-  }
-
-  componentWillUnmount() {
-
-  }
-
-  render() {
-    const {data} = this.state
-    const graphStyle = {
-      width: '100%',
-      height: '100%',
-      backgroundColor: 'blue'
+import ReactDOM from 'react-dom';
+import { VictoryBar, VictoryChart, VictoryAxis, VictoryTheme, VictoryStack  } from 'victory';
+  
+  
+ 
+  class BarGraph extends React.Component {
+    constructor() {
+      super()
+      this.state = {
+        data: {}
+      }
     }
 
-    const axisStyle = {
-      width: ``,
-      height: '100%',
-      backgroundColor: 'blue'
-    }
 
-    return (
-      <div className='graphDiv' style={graphStyle}>
-        <div className='axisDiv' style={axisStyle}>
-          
+    render() {
+      console.log(this.props)
+      
+      return (
+        <div>
+          <h1>Victory Tutorial</h1>
+          <VictoryChart
+            domainPadding={20}
+            theme={VictoryTheme.material} >
+            <VictoryAxis
+              tickValues={[1, 2, 3, 4]}
+              tickFormat={["Quarter 1", "Quarter 2", "Quarter 3", "Quarter 4"]}
+              />
+  
+            <VictoryAxis
+              dependentAxis
+              tickFormat={(x) => (`$${x / 1000}k`)}
+              />
+            <VictoryBar
+              data={this.props.data}
+              x='label'
+              y='value'
+            />
+  
+          </VictoryChart>
         </div>
-      </div>
-    );
+      );
+    }
   }
-}
+
+  // const app = document.getElementById('app');
+  // ReactDOM.render(<BarGraph />, app);
+
 
 export default BarGraph;
