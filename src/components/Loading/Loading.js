cec7481fe0d3c86664fbf3f7dc81327b258186bf
daniel70125 <dwrighttt504@gmail.com>
2020-07-21 11:14:46 -0500

    Updated

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-import React, {Component} from 'react';
-// import LoadingTransition from './LoadingTransition/LoadingTransition'
-import './Loading.css';
-
-
-
-class Loading extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
-    render() { 
-        return ( 
-            <div id='loading-cont'>
-                {/* <LoadingTransition /> */}
-                <h1>Hello</h1>
-            </div>
-         );
-    }
-=======
 import React, { useState } from 'react'
 // import Collapse from '@material-ui/core/Collapse'
 import Fade from '@material-ui/core/Fade'
@@ -292,7 +271,6 @@ const Loading = () => {
 			</Fade>
 		</>
 	)
->>>>>>> master
 }
 
 export default Loading
