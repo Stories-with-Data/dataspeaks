4c8f176bad6584fe649efc421bdc05d4b4df01a8
Eliottnibley <e.nibley@gmail.com>
2020-07-22 20:53:18 -0600

    update

--- src/components/GetInvolved/GetInvolvedFromNav.js
+++ src/components/GetInvolved/GetInvolvedFromNav.js
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from 'react'
-import { useHistory } from 'react-router-dom'
-import useStatesData from '../../hooks/useStatesData'
 import MaterialTable from 'material-table'
 import TwitterIcon from '@material-ui/icons/Twitter'
 import FacebookIcon from '@material-ui/icons/Facebook'
 import YouTubeIcon from '@material-ui/icons/YouTube'
+import MailIcon from '@material-ui/icons/Mail';
+import LanguageIcon from '@material-ui/icons/Language';
 import axios from 'axios'
-import IconButton from '@material-ui/core/IconButton'
 
 const GetInvolvedFromNav = () => {
 	// const data = useStatesData()
@@ -21,10 +20,8 @@ const GetInvolvedFromNav = () => {
     headers: {
     'X-API-KEY': 'sTgSE1HxTFvOD7NVYqQFCt32afEhu0ApzxLf4uav'
     }}).then(res => {
-
       setSenatorData(res.data.results[0].members)
       setIsLoading(false)
-      console.log(res.data.results[0].members)
     })}, [])
 
 
@@ -42,20 +39,12 @@ const GetInvolvedFromNav = () => {
 			field: 'state'
 		},
 		{
-			title: 'Twitter',
-      field: 'twitter_account'
+			title: 'Office Phone',
+      field: 'phone'
 		},
 		{
-			title: 'Facebook',
-			field: 'facebook_account'
-		},
-		{
-			title: 'YouTube',
-			field: 'youtube_account'
-		},
-		{
-			title: 'Contact Form',
-			field: 'overall.ranks.bpRank'
+			title: 'Senator Position',
+			field: 'state_rank'
 		}
 	]
 
@@ -78,25 +67,43 @@ const GetInvolvedFromNav = () => {
           },
           {
             icon: FacebookIcon,
-            tooltip: 'Facebook Account'
+            tooltip: 'Facebook Account',
+            onClick: (event, elem) => {
+              window.open(elem.facebook_account
+								? `https://www.facebook.com/${elem.facebook_account}`
+								: `https://www.facebook.com/search/top?q=senator%20${elem.first_name}%20${elem.last_name}`)
+            }
           },
           {
             icon: YouTubeIcon,
-            tooltip: 'YouTube Account'
+            tooltip: 'YouTube Account',
+            onClick: (event, elem) => {
+              window.open(elem.youtube_account
+								? `https://www.youtube.com/${elem.youtube_account}`
+								: `https://www.youtube.com/results?search_query=senator+${elem.first_name}+${elem.last_name}`)
+            }
+          },
+          {
+            icon: MailIcon,
+            tooltip: 'Contact Form',
+            onClick: (event, elem) => {
+              window.open(elem.contact_form)
+            }
+          },
+          {
+            icon: LanguageIcon,
+            tooltip: 'Website',
+            onClick: (event, elem) => {
+              window.open(elem.url)
+            }
           }
         ]
       }
 				options={{
-					pageSize: 101,
-					pageSizeOptions: [10, 25, 50, 101]
+					pageSize: 25,
+					pageSizeOptions: [25]
 				}}
 				title='US Senators'
-				// onRowClick={(event, rowData) => {
-				// 	history.push(
-				// 		`/states/${rowData.overall.stateName}`
-				// 	)
-				// }}
-				
 			/>
 		</>
 	)
